194bfa7806cf771aa1f2403680e2abccbd4a1a32
Lucimeri Andretta <luandretta@hotmail.com>
2023-06-22 07:36:19 +0000

    Feat: add profile serializer and list view

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
+from .serializers import ProfileSerializer
 
 
 class ProfileLIst(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
         return Response(profiles)
