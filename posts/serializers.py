9d4b9eee631c5f5bf245d40b185f176b4faf745d
Lucimeri Andretta <luandretta@hotmail.com>
2023-06-23 07:26:44 +0000

    Add PostList view

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from posts.models import Post
 
 
-class ProfileSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
---
+++ posts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from posts import views
+
+
+urlpatterns = [
+    path('posts/', views.PostList.as_view()),
+]
