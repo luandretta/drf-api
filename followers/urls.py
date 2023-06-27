cf297d6abdbd52413e15dab5df3f474772549038
Lucimeri Andretta <luandretta@hotmail.com>
2023-06-27 12:49:20 +0000

    Feat: add search into PostList
    Fix: typo in followers urls

--- followers/urls.py
+++ followers/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from followers import views
 
 urlpatterns = [
-    path('followers', views.Follower.as_view()),
-    path('followers/ < int:pk > /', views.FollowerDetail.as_view()),
+    path('followers', views.FollowerList.as_view()),
+    path('followers/<int:pk>/', views.FollowerDetail.as_view()),
 ]
