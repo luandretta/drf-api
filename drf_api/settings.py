5518bd339722bd47e2342b4ba3dc7a84aeabbd3b
Lucimeri Andretta <luandretta@hotmail.com>
2023-06-28 09:50:44 +0000

    Fix dj-rest-auth bug
    Add logout view

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import re
 import dj_database_url
+
 if os.path.exists('env.py'):
     import env
 
@@ -65,7 +67,23 @@ DEBUG = 'DEV' in os.environ
 ALLOWED_HOSTS = [
     '8000-luandretta-drfapi-ajdvx38ywrv.ws-eu100.gitpod.io',
     'localhost',
-    'https://django-rest-framework-andretta-f415d1e4c8be.herokuapp.com/']
+    'https://django-rest-framework-andretta-f415d1e4c8be.herokuapp.com/'
+]
+
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 
 # Application definition
@@ -109,17 +127,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
-    ]
-
-CORS_ALLOW_CREDENTIALS = True
-
 ROOT_URLCONF = 'drf_api.urls'
 
 TEMPLATES = [
@@ -151,17 +158,14 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
     }
 } """
 
-if 'DEV' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
+DATABASES = {
+    'default': ({
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    } if 'DEV' in os.environ else dj_database_url.parse(
+        os.environ.get('DATABASE_URL')
+    ))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
