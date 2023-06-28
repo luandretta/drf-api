ddd877d4645bf3ea0cc2956722ff9506a68c2034
Lucimeri Andretta <luandretta@hotmail.com>
2023-06-28 08:12:07 +0000

    Feat: add root_route message, pagination and date time format

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -72,8 +72,18 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y',
 }
+
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'restframework.renderers.JSONRenderer'
+    ]
+
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
